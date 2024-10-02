c6b880c72d6d2444f35b34953280663b798e03da
Lucius <kwaslukas@gmail.com>
2024-10-02 15:37:04 +0200

    dodane ikonki

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse,faDumbbell,faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Menu() {
   return (
@@ -7,9 +10,9 @@ export default function Menu() {
     <div className="fixed-bottom bg-light">
     
       <div className="d-flex justify-content-around">
-        <Link to="/" className="btn btn-primary w-100 text-center">Home</Link>
-        <Link to="/gym" className="btn btn-primary w-100 text-center">Gym</Link>
-        <Link to="/profile" className="btn btn-primary w-100 text-center">Profile</Link>
+        <Link to="/" className="btn btn-primary w-100 text-center"><FontAwesomeIcon icon={faHouse} /></Link>
+        <Link to="/gym" className="btn btn-primary w-100 text-center"><FontAwesomeIcon icon={faDumbbell} /></Link>
+        <Link to="/profile" className="btn btn-primary w-100 text-center"><FontAwesomeIcon icon={faUser} /></Link>
       </div>
     </div>
   );
